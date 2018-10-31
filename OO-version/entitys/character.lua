95715e45b970d83da304621b602eb4a72350fa74
felipefcomerlato <felipef1909@gmail.com>
2018-10-31 11:21:38 -0300

    Novos métodos do inimigo

--- OO-version/entitys/character.lua
+++ OO-version/entitys/character.lua
@@ -3,6 +3,7 @@ require("love.graphics")
 local character = {}
 
 function character.novo(texture, position_x, position_y)
+  print("Um novo PERSONAGEM foi criado")
   return {
     texture = love.graphics.newImage(texture),
     position_x = position_x,
