1f15f0fcab228c136fc5e293fdc59197dfe46d0b
felipefcomerlato <felipef1909@gmail.com>
2018-10-31 20:00:53 -0300

    Construction player entity

--- OO-version/entitys/player.lua
+++ OO-version/entitys/player.lua
@@ -1,13 +1,13 @@
-require("love.graphics")
+local character = require("entitys/character")
 
 local player = {}
+local texture = "images/baseshipb.png"
+local position_x = love.graphics.getWidth()/2
+local position_y = love.graphics.getHeight() - 50
 
-function player.novo(nome, position_x, position_y)
-  return {
-    texture = "baseshipb.png",
-    position_x = position_x,
-    position_y = position_y
-  }
+function player.new()
+  local player = character.new(texture, position_x, position_y, 20)
+  return player
 end
 
 return player
