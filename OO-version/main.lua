d4062eade1959c82c8e082c52c80df842ca2c54d
felipefcomerlato <felipef1909@gmail.com>
2018-10-31 11:22:08 -0300

    Draw através de métodos

--- OO-version/main.lua
+++ OO-version/main.lua
@@ -1,15 +1,15 @@
-local character = require("entitys/enemie")
+local enemie = require("entitys/enemie")
 
 
 function love.load()
-  enemie1 = enemie.novo("images/mysteryb.png",100,100)
-  enemie2 = enemie.novo("images/saucer3b.png",300,300)
+  table_of_enemies = enemie.makeTable()
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.draw(enemie1.texture, enemie1.position_x, enemie1.position_y)
-  love.graphics.draw(enemie2.texture, enemie2.position_x, enemie2.position_y)
+  for i=1,#table_of_enemies do
+      love.graphics.draw(table_of_enemies[i].texture, table_of_enemies[i].position_x, table_of_enemies[i].position_y)
+  end
 end
