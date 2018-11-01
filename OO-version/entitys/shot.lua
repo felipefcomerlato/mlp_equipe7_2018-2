a858d97a51eec3256cd486668cb2c8e63d48e4b9
felipefcomerlato <felipef1909@gmail.com>
2018-11-01 17:19:09 -0300

    Implementação do shot

--- OO-version/entitys/shot.lua
+++ OO-version/entitys/shot.lua
@@ -7,8 +7,24 @@ local speed = 5
 function shot.new(shooter)
   local shot = character.new(texture, shooter.position_x, shooter.position_y, speed)
 
-  function shot.move(self)
+  function shot.moveUp(self)
+    self:screenCollisionTest()
     self.position_y = self.position_y - speed
+  end
+
+  function shot.moveDown(self)
+    self.screenCollisionTest()
+    self.position_y = self.position_y + speed
+  end
+
+  function shot.screenCollisionTest(self)
+    if self.position_y <= character.getLimitScreen().top or self.position_y >= character.getLimitScreen().bottom then
+      self:destroy(shooter)
+    end
+  end
+
+  function shot.destroy(self, shooter)
+    shooter.shots[1] = nil
   end
 
   return shot
